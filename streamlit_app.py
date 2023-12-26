e7f3ad6c5a56206d3f5a60bdbf5a36693939ad08
prudhvirajb <154926721+prudhvirajb@users.noreply.github.com>
2023-12-26 15:58:24 -0500

    Update streamlit_app.py
    Updated the list with GIF

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberries Oat meal')
-streamlit.text('Kale, Spinach and Rocket smoothie')
-streamlit.text('Hard bolied Free-Range eggs')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberries Oat meal')
+streamlit.text('🥗 Kale, Spinach and Rocket smoothie')
+streamlit.text('🐔 Hard bolied Free-Range eggs')
+streamlit.text('🥑🍞 Avocado Toast')

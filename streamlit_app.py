875e6ea5b002e151ef75f7f7acd515bca77fc762
prudhvirajb <154926721+prudhvirajb@users.noreply.github.com>
2023-12-26 15:59:04 -0500

    Update streamlit_app.py
    New Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberries Oat meal')
 streamlit.text('🥗 Kale, Spinach and Rocket smoothie')
 streamlit.text('🐔 Hard bolied Free-Range eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

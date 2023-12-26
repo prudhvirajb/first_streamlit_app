cd177536f5892cc477cbfa6cb2b88e7230201829
prudhvirajb <154926721+prudhvirajb@users.noreply.github.com>
2023-12-26 15:55:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberries Oat meal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Hard bolied Free-Range eggs')
